5736c98d47f15a5a8733c444b0017707a5a85a6e
Ivailo97 <ivailo.8.1993@abv.bg>
2019-10-24 17:40:23 +0300

    project details page updated..

--- src/main/java/dreamcompany/domain/model/view/ProjectDetailsViewModel.java
+++ src/main/java/dreamcompany/domain/model/view/ProjectDetailsViewModel.java
@@ -7,6 +7,8 @@ public class ProjectDetailsViewModel {
 
     private String id;
 
+    private String name;
+
     private String description;
 
     private BigDecimal reward;
@@ -43,5 +45,13 @@ public class ProjectDetailsViewModel {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
