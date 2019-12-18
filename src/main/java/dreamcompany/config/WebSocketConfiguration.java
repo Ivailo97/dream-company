b4b749f499800426be69f14540348e934b1e8c60
Ivailo97 <ivailo.8.1993@abv.bg>
2019-12-18 21:36:41 +0200

    u

--- src/main/java/dreamcompany/config/WebSocketConfiguration.java
+++ src/main/java/dreamcompany/config/WebSocketConfiguration.java
@@ -12,7 +12,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocketApp").withSockJS();
+        registry.addEndpoint("https://dream-company.herokuapp.com/websocketApp").withSockJS();
     }
 
     @Override
